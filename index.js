592eaa1894f9098e86faf5f19e5b629516ce0f48
AyushmaanRajput <130346560+AyushmaanRajput@users.noreply.github.com>
2023-10-28 22:45:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
-app.use(express.json());
 //** Routes */
 app.use("/users", userRoutes);
 app.use("/notes", notesRoutes);
